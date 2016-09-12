75b25ea90b3cf0d7b5bd8a646b03f9c62a88b031
julianoezequiel@gmail.com <julianoezequiel@gmail.com>
2016-09-12 19:19:24 -0300

    email

--- email.php
+++ email.php
@@ -1,28 +1,28 @@
 <?php
 
-	$nome      = $_POST['nome']; 
+	  $nome      = $_POST['nome'];
     $email     = $_POST['email'];
     $comments  = $_POST['comments'];
-	$tel  = $_POST['tel'];
+	  $tel  = $_POST['tel'];
 
-	  
+
     $headers  = "From: $nome \r\n";
     $headers .= "Reply-To: $email\r\n";
- 
+
 /*abaixo será os dados que serão enviado para o email
 cadastrado para receber o formulário.*/
- 
+
        $corpo = "Contato Vidros Curitiba \n";
        $corpo .= "Nome: " . $nome . "\n";
-	   $corpo .= "Telefone: " . $tel . "\n";
+	     $corpo .= "Telefone: " . $tel . "\n";
        $corpo .= "Email: " . $email . "\n";
        $corpo .= "Comentario: " . $comments . "\n";
-	  	
-       $email_to = 'vidroscuritiba@hotmail.com'; //Email para onde vai ser enviado
+
+       $email_to = 'vidroscuritiba@hotmail.com;julianoezequiel@gmail.com'; //Email para onde vai ser enviado
 	   //julianoezequiel@gmail.com
- 
+
     $status = mail($email_to, "Contato Site", $corpo, $headers); //enviando o email.
- 
+
     if($status) {
         echo "<script> alert('Email enviando com sucesso!'); </script>"; //verifica se foi enviado o email com sucesso.
     }
